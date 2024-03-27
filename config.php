ec311da8f3722a25f07efb4b8ed7cbc41ebb8791
Gus Mueller <gus@asecular.com>
2024-03-27 12:35:38 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "weather";
+$database = "weathertron";
+$password = "xxxxxx";
+ 
+
+$encryptionPassword = "xxxxx";
+$cookiename = "whatever";
+$poserCookieName = "poser_id";
