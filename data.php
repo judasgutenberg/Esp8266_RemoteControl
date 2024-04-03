53e9ac7cecbb9c102edc1387a8132fb8ea39c903
Gus Mueller <gus@asecular.com>
2024-04-03 12:05:21 -0400

    Update data.php

--- data.php
+++ data.php
@@ -18,20 +18,27 @@ $out = [];
 $date = new DateTime("now", new DateTimeZone('America/New_York'));//obviously, you would use your timezone, not necessarily mine
 $formatedDateTime =  $date->format('Y-m-d H:i:s');
 //$formatedDateTime =  $date->format('H:i');
-
+$deviceId = "";
+$locationId = "";
 if($_REQUEST) {
 	if(array_key_exists("mode", $_REQUEST)) {
 		$mode = $_REQUEST["mode"];
 	} else {
 		$mode = "getData";
 	}
-	//we had been passing in a locationId but we have since 
-	//changed that to deviceId. locationId is to support
-	//the devices still passing in locationId
+	//i may end up deciding that locationId and deviceId are the same thing, and for now they are
+	//but maybe some day they will be different things
 	if(array_key_exists("locationId", $_REQUEST)) {
-		$deviceId = $_REQUEST["locationId"];
-	} else if (array_key_exists("deviceId", $_REQUEST)) {
+		$locationId = $_REQUEST["locationId"];
+	}
+	if (array_key_exists("deviceId", $_REQUEST)) {
 		$deviceId = $_REQUEST["deviceId"];
+	}
+	if(!$locationId) {
+		$locationId =  $deviceId;
+	}
+	if(!$deviceId) {
+		$deviceId = $locationId;
 	}
 	if(!$conn) {
         $out = ["error"=>"bad database connection"];
@@ -45,162 +52,169 @@ if($_REQUEST) {
 			$lines = [];
 			$arrWeatherData = [0,0,0,0,0,0,0,0,0,0,0,0];
 		}
-		if($mode=="kill") {
-			$method  = "kill";
-		
-		} else if ($mode=="getDeviceData") {
+		$canAccessData = array_key_exists("storagePassword", $_REQUEST) && $storagePassword == $_REQUEST["storagePassword"];
 
-
-
-		} else if ($mode=="getData") {
-			if(array_key_exists("scale", $_REQUEST)) {
-				$scale = $_REQUEST["scale"];
-			} else {
-				$scale = "";
-			}
+		if($canAccessData) {
+			if($mode=="kill") {
+				$method  = "kill";
 			
-			if(!$conn) {
-				$out = ["error"=>"bad database connection"];
-			} else {
-				
-				if($scale == ""  || $scale == "fine") {
-					$sql = "SELECT * FROM " . $database . ".weather_data  
-					WHERE recorded > DATE_ADD(NOW(), INTERVAL -1 DAY) AND location_id=" . $deviceId . " 
-					ORDER BY weather_data_id ASC";
+			} else if ($mode=="getDeviceData") {
+
+
+
+			} else if ($mode=="getData") {
+				if(array_key_exists("scale", $_REQUEST)) {
+					$scale = $_REQUEST["scale"];
 				} else {
-					if($scale == "hour") {
-						$sql = "SELECT
-						*,
-						YEAR(recorded), DAYOFYEAR(recorded), HOUR(recorded) FROM " . $database . ".weather_data  
-						WHERE recorded > DATE_ADD(NOW(), INTERVAL -7 DAY) AND location_id=" . $deviceId . " 
-							GROUP BY YEAR(recorded), DAYOFYEAR(recorded), HOUR(recorded)
-							ORDER BY weather_data_id ASC";
-					}
-					if($scale == "day") {
-						$sql = "SELECT 	 
-						*,
-						YEAR(recorded), DAYOFYEAR(recorded) FROM " . $database . ".weather_data  
-						WHERE location_id=" . $deviceId . " 
-							GROUP BY YEAR(recorded), DAYOFYEAR(recorded)
-							ORDER BY weather_data_id ASC";
-					}
+					$scale = "";
 				}
-				/*
-				//using averages didn't work for some reason:
-				weather_data_id, 
-				recorded, 
-				AVG(temperature) AS temperature, 
-				AVG(pressure) AS pressure, 
-				AVG(humidity) AS humidity, 
-				wind_direction, 
-				AVG(precipitation) AS precipitation, 
-				wind_increment, 
-				*/
-				//echo $sql;
-				$result = mysqli_query($conn, $sql);
-				$out = [];
-				if($result) {
-					while($row = mysqli_fetch_array($result)) {
-						array_push($out, $row);
-					}
-				}
-			}
-			$method  = "read";	
-		} else if ($mode == "saveData") { //save data
-		//test url;:
-		// //http://randomsprocket.com/weather/data.php?storagePassword=butterfly&locationId=3&mode=saveData&data=10736712.76*12713103.20*1075869.28*NULL|0*0*1710464489*1710464504*1710464519*1710464534*1710464549*1710464563*1710464579*1710464593*
-
-			
-			
-			
-			$temperature = $arrWeatherData[0];
-			$pressure = $arrWeatherData[1];
-			$humidity = $arrWeatherData[2];
-			$gasMetric = "NULL";
-			if(count($arrWeatherData)>3) {
-			$gasMetric = $arrWeatherData[3];
-			}
-			
-			//select * from weathertron.weather_data where location_id=3 order by recorded desc limit 0,10;
-			$weatherSql = "INSERT INTO weather_data(location_id, recorded, temperature, pressure, humidity, gas_metric, wind_direction, precipitation, wind_speed, wind_increment) VALUES (" . 
-			mysqli_real_escape_string($conn, $deviceId) . ",'" .  
-			mysqli_real_escape_string($conn, $formatedDateTime)  . "'," . 
-			mysqli_real_escape_string($conn, $temperature) . "," . 
-			mysqli_real_escape_string($conn, $pressure) . "," . 
-			mysqli_real_escape_string($conn, $humidity) . "," . 
-			mysqli_real_escape_string($conn, $gasMetric) .
-			",NULL,NULL,NULL,NULL)";
-			//echo $weatherSql;
-
-			if($temperature != "NULL") { //if temperature is null, do not attempt to store!
-				if(array_key_exists("storagePassword", $_REQUEST) && $storagePassword == $_REQUEST["storagePassword"]) { //prevents malicious data corruption
-				$result = mysqli_query($conn, $weatherSql);
-				}
-			}
-			$method  = "insert";
-			$out = Array("message" => "done", "method"=>$method);
-		
-
-		}
-		if($mode == "getDeviceData" || $mode == "saveData" ) {
-			$pinValuesKnownToDevice = [];
-			$specificPin = -1;
-			if(count($lines)>1) {
-				$recentReboots = explode("*", $lines[1]);
-				foreach($recentReboots as $rebootOccasion) {
-					if(intval($rebootOccasion) > 0 && array_key_exists("storagePassword", $_REQUEST) && $storagePassword == $_REQUEST["storagePassword"]) {
-					$dt = new DateTime();
-					$dt->setTimestamp($rebootOccasion);
-					$rebootOccasionSql = $dt->format('Y-m-d H:i:s');
-					$rebootLogSql = "INSERT INTO reboot_log(location_id, recorded) SELECT " . intval($deviceId) . ",'" .$rebootOccasionSql . "' 
-						FROM DUAL WHERE NOT EXISTS (SELECT * FROM reboot_log WHERE location_id=" . intval($deviceId) . " AND recorded='" . $rebootOccasionSql . "' LIMIT 1)";
+				
+				if(!$conn) {
+					$out = ["error"=>"bad database connection"];
+				} else {
 					
-					$result = mysqli_query($conn, $rebootLogSql);
-					}
-				}
-				if(count($lines) > 2) {
-					$pinValuesKnownToDevice = explode("*", $lines[2]);
-
-				}
-				if(count($lines) > 3) {
-					$extraInfo = explode("*", $lines[3]);
-					if(count($extraInfo)>1){
-						$lastCommandId = $extraInfo[0];
-						
-						$specificPin = $extraInfo[1];
-					}
-				}
-			
-			} 
-
-			//the part where we include any data from our remote control system:
-			$deviceSql = "SELECT pin_number, value, enabled, can_be_analog, device_feature_id  FROM device_feature f LEFT JOIN device_type_feature t ON f.device_type_feature_id=t.device_type_feature_id WHERE device_id=" . intval($deviceId) . ";";
-			//echo $deviceSql;
-			$result = mysqli_query($conn, $deviceSql);
-			if($result) {
-				$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-				$pinCursor = 0;
-				foreach($rows as $row) {
-					$pinNumber = $row["pin_number"];
-					if($specificPin == -1 || $specificPin ==  $pinCursor){
-						if(count($pinValuesKnownToDevice)>0) {
-							//this part update device_feature so we can tell from the server if the device has taken on the server's value
-							$sqlToUpdateDeviceFeature = "UPDATE device_feature SET last_known_device_value =  " . $pinValuesKnownToDevice[$pinCursor];
-							$sqlToUpdateDeviceFeature .= ", last_known_device_modified='" . $formatedDateTime . "'";
-							$sqlToUpdateDeviceFeature .= " WHERE device_feature_id=" . $row["device_feature_id"];
-							//echo $sqlToUpdateDeviceFeature  . "<BR>";
-							$updateResult = mysqli_query($conn, $sqlToUpdateDeviceFeature);
+					if($scale == ""  || $scale == "fine") {
+						$sql = "SELECT * FROM " . $database . ".weather_data  
+						WHERE recorded > DATE_ADD(NOW(), INTERVAL -1 DAY) AND location_id=" . $locationId . " 
+						ORDER BY weather_data_id ASC";
+					} else {
+						if($scale == "hour") {
+							$sql = "SELECT
+							*,
+							YEAR(recorded), DAYOFYEAR(recorded), HOUR(recorded) FROM " . $database . ".weather_data  
+							WHERE recorded > DATE_ADD(NOW(), INTERVAL -7 DAY) AND location_id=" . $locationId . " 
+								GROUP BY YEAR(recorded), DAYOFYEAR(recorded), HOUR(recorded)
+								ORDER BY weather_data_id ASC";
 						}
-						unset($row["device_feature_id"]);//make things as lean as possible for IoT device
-						$out["device_data"][] = $row;
+						if($scale == "day") {
+							$sql = "SELECT 	 
+							*,
+							YEAR(recorded), DAYOFYEAR(recorded) FROM " . $database . ".weather_data  
+							WHERE location_id=" . $locationId . " 
+								GROUP BY YEAR(recorded), DAYOFYEAR(recorded)
+								ORDER BY weather_data_id ASC";
+						}
 					}
-					$out["pin_list"][] = $pinNumber ;
-					$pinCursor++;
+					/*
+					//using averages didn't work for some reason:
+					weather_data_id, 
+					recorded, 
+					AVG(temperature) AS temperature, 
+					AVG(pressure) AS pressure, 
+					AVG(humidity) AS humidity, 
+					wind_direction, 
+					AVG(precipitation) AS precipitation, 
+					wind_increment, 
+					*/
+					//echo $sql;
+					$result = mysqli_query($conn, $sql);
+					$out = [];
+					if($result && $canAccessData) {
+						while($row = mysqli_fetch_array($result)) {
+							array_push($out, $row);
+						}
+					}
 				}
-			}
+				$method  = "read";	
+			} else if ($mode == "saveData") { //save data
+			//test url;:
+			// //http://randomsprocket.com/weather/data.php?storagePassword=butterfly&locationId=3&mode=saveData&data=10736712.76*12713103.20*1075869.28*NULL|0*0*1710464489*1710464504*1710464519*1710464534*1710464549*1710464563*1710464579*1710464593*
+
+				
+				
+				
+				$temperature = $arrWeatherData[0];
+				$pressure = $arrWeatherData[1];
+				$humidity = $arrWeatherData[2];
+				$gasMetric = "NULL";
+				if(count($arrWeatherData)>3) {
+				$gasMetric = $arrWeatherData[3];
+				}
+				
+				//select * from weathertron.weather_data where location_id=3 order by recorded desc limit 0,10;
+				$weatherSql = "INSERT INTO weather_data(location_id, recorded, temperature, pressure, humidity, gas_metric, wind_direction, precipitation, wind_speed, wind_increment) VALUES (" . 
+				mysqli_real_escape_string($conn, $locationId) . ",'" .  
+				mysqli_real_escape_string($conn, $formatedDateTime)  . "'," . 
+				mysqli_real_escape_string($conn, $temperature) . "," . 
+				mysqli_real_escape_string($conn, $pressure) . "," . 
+				mysqli_real_escape_string($conn, $humidity) . "," . 
+				mysqli_real_escape_string($conn, $gasMetric) .
+				",NULL,NULL,NULL,NULL)";
+				//echo $weatherSql;
+
+				if($temperature != "NULL") { //if temperature is null, do not attempt to store!
+					if(true) { //prevents malicious data corruption
+						$result = mysqli_query($conn, $weatherSql);
+					}
+				}
+				$method  = "saveWeatherData";
+				$out = Array("message" => "done", "method"=>$method);
 			
-		}
+
+			}
+			if($mode == "getDeviceData" || $mode == "saveData" ) {
+				$method = "getDeviceData";
+				$pinValuesKnownToDevice = [];
+				$specificPin = -1;
+				if(count($lines)>1) {
+					$recentReboots = explode("*", $lines[1]);
+					foreach($recentReboots as $rebootOccasion) {
+						if(intval($rebootOccasion) > 0 && array_key_exists("storagePassword", $_REQUEST) && $storagePassword == $_REQUEST["storagePassword"]) {
+						$dt = new DateTime();
+						$dt->setTimestamp($rebootOccasion);
+						$rebootOccasionSql = $dt->format('Y-m-d H:i:s');
+						$rebootLogSql = "INSERT INTO reboot_log(location_id, recorded) SELECT " . intval($deviceId) . ",'" .$rebootOccasionSql . "' 
+							FROM DUAL WHERE NOT EXISTS (SELECT * FROM reboot_log WHERE location_id=" . intval($deviceId) . " AND recorded='" . $rebootOccasionSql . "' LIMIT 1)";
+						
+						$result = mysqli_query($conn, $rebootLogSql);
+						}
+					}
+					if(count($lines) > 2) {
+						$pinValuesKnownToDevice = explode("*", $lines[2]);
+
+					}
+					if(count($lines) > 3) {
+						$extraInfo = explode("*", $lines[3]);
+						if(count($extraInfo)>1){
+							$lastCommandId = $extraInfo[0];
+							
+							$specificPin = $extraInfo[1];
+						}
+					}
+				
+				} 
+
+				//the part where we include any data from our remote control system:
+				$deviceSql = "SELECT pin_number, value, enabled, can_be_analog, device_feature_id  FROM device_feature f LEFT JOIN device_type_feature t ON f.device_type_feature_id=t.device_type_feature_id WHERE device_id=" . intval($deviceId) . ";";
+				//echo $deviceSql;
+				$result = mysqli_query($conn, $deviceSql);
+				if($result) {
+					$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+					$pinCursor = 0;
+					foreach($rows as $row) {
+						$pinNumber = $row["pin_number"];
+						if($specificPin == -1 || $specificPin ==  $pinCursor){
+							if(count($pinValuesKnownToDevice)>0) {
+								//this part update device_feature so we can tell from the server if the device has taken on the server's value
+								$sqlToUpdateDeviceFeature = "UPDATE device_feature SET last_known_device_value =  " . $pinValuesKnownToDevice[$pinCursor];
+								$sqlToUpdateDeviceFeature .= ", last_known_device_modified='" . $formatedDateTime . "'";
+								$sqlToUpdateDeviceFeature .= " WHERE device_feature_id=" . $row["device_feature_id"];
+								//echo $sqlToUpdateDeviceFeature  . "<BR> " . $specificPin  . "<BR>";
+								$updateResult = mysqli_query($conn, $sqlToUpdateDeviceFeature);
+							}
+							unset($row["device_feature_id"]);//make things as lean as possible for IoT device
+							$out["device_data"][] = $row;
+						}
+						$out["pin_list"][] = $pinNumber ;
+						$pinCursor++;
+					}
+				}
+				
+			} 
+		}else {
+			$out = ["error"=>"you lack permissions"];
 	}
+}
 	echo json_encode($out);
 	
 	
