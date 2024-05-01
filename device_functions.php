ab8dfb119e7108a399d4dacb59c610f0e913baf8
Gus Mueller <gus@asecular.com>
2024-05-01 17:02:13 -0400

    added functionality to get data from the SolArk cloud

--- device_functions.php
+++ device_functions.php
@@ -33,10 +33,18 @@ function deviceFeatures($userId, $deviceId) {
       'name' => 'pin_number' 
     ],
     [
+      'label' => 'device type feature id',
+      'name' => 'device_type_feature_id' 
+    ],
+    [
       'label' => 'power on',
       'name' => 'value',
       'liveChangeable' => true,
       'type' => 'bool'
+    ],
+    [
+      'label' => 'device feature id',
+      'name' => 'device_feature_id' 
     ],
     [
       'label' => 'created',
@@ -88,7 +96,11 @@ function devices($userId) {
     [
       'label' => 'location name',
       'name' => 'location_name' 
-    ] 
+    ],
+    [
+      'label' => 'ip address',
+      'name' => 'ip_address' 
+    ],
     );
     $toolsTemplate = "<a href='?table=" . $table . "&" . $table . "_id=<" . $table . "_id/>'>Edit Info</a> ";
     $toolsTemplate .= " | <a href='?table=device_feature&device_id=<" . $table . "_id/>'>Device Features</a>";
@@ -102,4 +114,87 @@ function devices($userId) {
     }
     return $out;
 
+}
+
+
+//reads data from the cloud about our particular solar installation
+function getCurrentSolarData($user) {
+    $plantId = $user["energy_api_plant_id"];
+    $url = 'https://pv.inteless.com/oauth/token';
+    $headers = [
+            'Content-Type: application/json;charset=UTF-8', // Set Content-Type header to application/json
+            'accept: application/json',
+            'Sec-Fetch-Mode: cors',
+            'Origin: https://pv.inteless.com',
+            'Accept: application/json',
+            'Accept-Encoding:   ',
+            'Accept-Language: en-US,en;q=0.9',
+            'Sec-Ch-Ua: "Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+            'Sec-Ch-Ua-Platform: Windows',
+            'Sec-Ch-Ua-Mobile: ?0',
+            'Priority: u=1, i',
+            'Referer: https://pv.inteless.com/login',
+            'Content-Length: 127',
+            'Content-Type: application/json;charset=UTF-8',
+            'Sec-Fetch-Dest: empty',
+            'Sec-Fetch-Mode: cors',
+            'Sec-Fetch-Site: same-origin',
+            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    ];
+    $params = [
+            'client_id' => 'csp-web',
+            'grant_type' => 'password',
+            'password' => $user["energy_api_password"],
+            'username' => $user["energy_api_username"],
+            'source' => 'elinter',
+    ];
+
+    $ch = curl_init();
+    // Set cURL options
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); // Set custom headers
+
+    $response = curl_exec($ch);
+
+    if(curl_errno($ch)){
+        echo 'Curl error: ' . curl_error($ch);
+    }
+
+    curl_close($ch);
+
+    try {
+        $bodyData = json_decode($response, true);
+        $data = $bodyData["data"];
+        $access_token = $data['access_token'];
+        $currentDateTime = new DateTime('now', new DateTimeZone('America/New_York')); 
+        //echo $currentDateTime->format('Y-m-d h:i:s');
+        $currentDate =  $currentDateTime->format('Y-m-d');
+        $actionUrl = 'https://pv.inteless.com/api/v1/plant/energy/' . $plantId  . '/day?date=' . $currentDate . "&id=" . $plantId . "&lan=en";
+        $actionUrl = 'https://pv.inteless.com/api/v1/plant/energy/' . $plantId  . '/flow?date=' . $currentDate . "&id=" . $plantId . "&lan=en"; 
+        $actionUrl = 'https://pv.inteless.com/api/v1/plant/energy/' . $plantId  . '/flow';
+        $userParams =   [
+                'access_token' => $access_token,
+                'date' => $currentDate,
+                'id' => 16588,
+                'lan' => 'en'         
+        ];
+
+        // Make GET request to user endpoint
+        $queryString = http_build_query($userParams);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $actionUrl . "?" . $queryString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $dataResponse = curl_exec($ch);
+        curl_close($ch);
+
+        $dataBody = json_decode($dataResponse, true);
+
+        return $dataBody["data"];
+    } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+        //return $e->getResponse()->getBody()->getContents();
+    }
 }
