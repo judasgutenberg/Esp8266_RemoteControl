78d60611b559f8586cbdd68ab92e5367516b58f9
Gus Mueller <gus@asecular.com>
2024-06-05 09:49:58 -0400

    building in support for multiple sensors

--- config.h
+++ config.h
@@ -7,7 +7,8 @@ extern const char* storagePassword; //to ensure someone doesn't store bogus data
 //url will be in the form: http://your-server.com:80/weather/data.php?data=
 extern const char* urlGet;
 extern const char* hostGet;
-extern const int sensorType; //SENSORS! -- we support these: 180 for BMP180, 2301 for DHT 2301, 680 for BME680.  0 for no sensor.  No support for multiple sensors for now.
+extern const int sensorType; //SENSORS! -- we support these: 180 for BMP180, 2301 for DHT 2301, 680 for BME680.  0 for no sensor. 
+extern const int sensorI2C;
 extern const int locationId; //3 really is watchdog
 extern const int pollingGranularity; //how often to poll backend in seconds, 4 makes sense
 extern const int dataLoggingGranularity; //how often to store data in backend, 300 makes sense
@@ -21,9 +22,7 @@ extern const int hotspotLimitedTimeFrame; //seconds
 extern const int hotspotLimitedTimeFrame; //seconds
 extern const int moxeePowerSwitch; //usually 14
 extern const int deepSleepTimePerLoop; //seconds. doesn't yet work
-extern const int dhtData;  
-extern const int dhtPower;  
-// #define dhType DHT11 // DHT 11
-// #define dhType DHT22 // DHT 22, AM2302, AM2321
-extern const int dhtType;
+extern const int sensorData;  
+extern const int sensorPower;  
+extern const int sensorSubType;
 extern const char pinsToStartLow[];
