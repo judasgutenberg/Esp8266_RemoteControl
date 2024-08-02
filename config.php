2df548998d8541165bb1b9b9125892c707596d0a
Gus Mueller <gus@asecular.com>
2024-08-02 12:19:17 -0400

    had to make config

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$servername = "localhost";
+$username = "remote";
+$database = "remote";
+$password = "your_awesome_password";
+
+
+$encryptionPassword = "your secret just for you";
+$cookiename = "email";
+$poserCookieName = "poser_id";
+$tenantCookieName = "tenant";
