9a9cb130c4c7f97e222f3fc13da817698be4c0b4
Gus Mueller <gus@asecular.com>
2024-12-21 17:43:05 -0500

    Update config.c

--- config.c
+++ config.c
@@ -33,5 +33,5 @@ const int sensor_power_pin = 12;
 // sensor_sub_type DHT22 // DHT22, AM2302, AM2321 = 22 or 21
 const int sensor_sub_type = 21; // DHT 21, AM2301
 const int ir_pin = -1; //set to the value of an actual data pin if you want to send data via ir from your controller on occasion
-
+const int ina219_address = 0x40; //set to -1 if you have no power voltage to monitor
 const char pins_to_start_low[] = {12, 13, -1}; //so when the device comes up it doesn't immediately turn on attached devices
