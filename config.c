e1b41b152c665d618b339c57216f29df48f6b12b
Gus Mueller <gus@asecular.com>
2024-12-25 16:45:52 -0500

    now does light sleep

--- config.c
+++ config.c
@@ -23,6 +23,7 @@ const int hotspot_limited_time_frame = 340; //seconds
 const int moxee_power_switch = 13; //usually 14 set to -1 if not used
 
 int deep_sleep_time_per_loop = 0;  //in seconds. saves energy.  set to zero if unneeded. GPIO16/D0 needs to be conneted to RST in hardware first. can be changed remotely
+int light_sleep_time_per_loop = 0;
 
 //if you are using a DHT hygrometer/temperature probe, these values will be important
 //particularly if you reflashed a MySpool temperature probe (https://myspool.com/) with custom firmware
