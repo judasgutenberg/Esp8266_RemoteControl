8ecac17b06bf1ab423f81e3c2567e05931bf1fd2
Gus Mueller <gus@asecular.com>
2025-04-01 11:00:12 -0400

    Update config.h

--- config.h
+++ config.h
@@ -3,7 +3,7 @@
 extern const char* wifi_ssid; //mine was Moxee Hotspot83_2.4G
 extern const char* wifi_password;
 extern const char* storage_password; //to ensure someone doesn't store bogus data on your server. should match value in config.php
-extern const unsigned long long encryption_scheme;
+extern const unsigned long long encryption_scheme; //needs sixteen characters of hexadecimal 
 //data posted to remote server so we can keep a historical record
 //url will be in the form: http://your-server.com:80/weather/data.php?data=
 extern const char* url_get;
