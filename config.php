8ac9618423b36b4940e9f7294089c245689fc915
Gus Mueller <gus@asecular.com>
2025-05-02 06:41:07 -0400

    Update config.php for external sendmail

--- config.php
+++ config.php
@@ -13,3 +13,7 @@ $cookiename = "email";
 $poserCookieName = "poser_id";
 $tenantCookieName = "tenant";
 $backupLocation = "./someplace_secret_or_perhaps_not_in_your_web_tree";
+
+//these are for cases where you need some other server to provide send email functionality using an externalmailer
+$remoteEmailPassword = "your_external_mailer_password";
+$remoteEmailUrl = "http://your_other_domain.com/externalmailer.php";
