8cae59f20caf0efdfdab65beae3eccb30f3f3f7e
Gus Mueller <gus@asecular.com>
2025-05-31 21:31:24 -0400

    Update index.h

--- index.h
+++ index.h
@@ -202,7 +202,7 @@ function showPinValues(){
    xhttp.send();
    setTimeout(function(){
       updateWeatherDisplay(); 
-      }, Math.random()* 4000); //i make this a little non-deterministic so the DHT sensors won't get stuck in a pattern where they are read to frequently
+      }, Math.random()* 4000); //i make this a little non-deterministic so the DHT sensors won't get stuck in a pattern where they are read too frequently
 }
 </script>
 </body>
