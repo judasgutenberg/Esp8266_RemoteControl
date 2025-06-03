beb40e558623ba52fffad2c037e1a18352910e33
Gus Mueller <gus@asecular.com>
2025-06-02 20:13:37 -0400

    made feature.php more phone-friendly

---
+++ dashboard.js
@@ -0,0 +1,78 @@
+function toggleDeviceFeature(deviceFeatureId, hashedEntries,  actuallySetState) {
+  state = !state;
+  const value = state ? 'true' : 'false';
+  let url = 'tool.php?action=genericFormSave&table=device_feature&primary_key_name=device_feature_id&primary_key_value=' + deviceFeatureId + '&value=' + value + '&name=value&hashed_entities=' + hashedEntries;
+  if(!actuallySetState) {
+    url = "data:application/json,{}";
+    state = !state;  //oh, we didn't send a change state, so flip it back to the way things were
+  }
+  let bgColor = "#ccccff";
+  
+  let urlGet = 'tool.php?action=json&table=device_feature';
+  if(deviceFeatureId) {
+    urlGet += '&device_feature_id=' + deviceFeatureId;
+  }
+  console.log(urlGet);
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        fetch(urlGet).then(response => {
+          if(response.ok) {
+            console.log(response);
+            return response.json();
+
+          }
+        }).then(data => {
+            console.log(data);
+            if("value" in data) {
+              state = changeButton(data)
+            } else {
+              Object.keys(data).forEach(key => {
+                state = changeButton(data[key])
+              });
+            }
+        });
+      } else {
+        //alert('Failed to send command');
+      }
+    })
+    .catch(err => alert('Error: ' + err));
+
+}
+
+function changeButton(data) {
+bgColor = '#cccccc';
+stateDisplay = "Unknown or non-existent";
+if(data["value"] === "1") {
+  state = true;
+  stateDisplay = "Just Switched ON";
+  bgColor = "#ffe7cc"
+} else if (data["value"] === "0"){
+  state = false;
+  stateDisplay = "Just Switched OFF";
+  bgColor = "#e7e7ff"
+}
+if(data["last_known_device_value"] === "1" && data["value"] === "1") {
+  bgColor = "#ffffcc"
+  stateDisplay = "Confirmed ON";
+}
+if(data["last_known_device_value"] === "0" && data["value"] === "0") {
+  bgColor = "#ccccff"
+  stateDisplay = "Confirmed OFF";
+}
+let deviceFeatureId = data["device_feature_id"];
+if(document.getElementById("statedisplay_" + deviceFeatureId)) {
+  document.getElementById("statedisplay_" + deviceFeatureId).innerHTML = stateDisplay;
+  document.getElementById("toggleButton_" + deviceFeatureId).style.backgroundColor  = bgColor;
+  document.getElementById("toggleButton_" + deviceFeatureId).innerHTML = data["name"];
+}
+return state
+}
+
+function updateFeatureDetailButtons() {
+  setTimeout(()=>{
+  toggleDeviceFeature(null, null,  false);
+  updateFeatureDetailButtons();
+  },
+  5000);
+}
--- feature.php
+++ feature.php
@@ -47,96 +47,12 @@ if(!$user) {
 <html>
 <head>
   <title>Device Feature Toggle</title>
-  <link rel='stylesheet' href='tool.css?version=1711570359'>
+  <link rel='stylesheet' href='dashboard.css?version=1711570359'>
   <script src='tool.js'></script>
+  <script src='dashboard.js'></script>
   <link rel="icon" type="image/x-icon" href="./favicon.ico" />
 </head>
-
 <body>
-<script>
-  function toggleDeviceFeature(deviceFeatureId, hashedEntries,  actuallySetState) {
-      state = !state;
-      const value = state ? 'true' : 'false';
-      let url = 'tool.php?action=genericFormSave&table=device_feature&primary_key_name=device_feature_id&primary_key_value=' + deviceFeatureId + '&value=' + value + '&name=value&hashed_entities=' + hashedEntries;
-      if(!actuallySetState) {
-        url = "data:application/json,{}";
-        state = !state;  //oh, we didn't send a change state, so flip it back to the way things were
-      }
-      let bgColor = "#ccccff";
-      
-      let urlGet = 'tool.php?action=json&table=device_feature';
-      if(deviceFeatureId) {
-        urlGet += '&device_feature_id=' + deviceFeatureId;
-      }
-      console.log(urlGet);
-      fetch(url)
-        .then(response => {
-          if (response.ok) {
-            fetch(urlGet).then(response => {
-              if(response.ok) {
-                console.log(response);
-                return response.json();
-
-              }
-            }).then(data => {
-                console.log(data);
-                if("value" in data) {
-                  state = changeButton(data)
-                } else {
-                  Object.keys(data).forEach(key => {
-                    state = changeButton(data[key])
-                  });
-                }
-            });
-          } else {
-            //alert('Failed to send command');
-          }
-        })
-        .catch(err => alert('Error: ' + err));
-  
- }
- 
- function changeButton(data) {
-    bgColor = '#cccccc';
-    stateDisplay = "Unknown or non-existent";
-    if(data["value"] === "1") {
-      state = true;
-      stateDisplay = "Just Switched ON";
-      bgColor = "#ffe7cc"
-    } else if (data["value"] === "0"){
-      state = false;
-      stateDisplay = "Just Switched OFF";
-      bgColor = "#e7e7ff"
-    }
-    if(data["last_known_device_value"] === "1" && data["value"] === "1") {
-      bgColor = "#ffffcc"
-      stateDisplay = "Confirmed ON";
-    }
-    if(data["last_known_device_value"] === "0" && data["value"] === "0") {
-      bgColor = "#ccccff"
-      stateDisplay = "Confirmed OFF";
-    }
-    let deviceFeatureId = data["device_feature_id"];
-    if(document.getElementById("statedisplay_" + deviceFeatureId)) {
-      document.getElementById("statedisplay_" + deviceFeatureId).innerHTML = stateDisplay;
-      document.getElementById("toggleButton_" + deviceFeatureId).style.backgroundColor  = bgColor;
-      document.getElementById("toggleButton_" + deviceFeatureId).innerHTML = data["name"];
-    }
-    return state
- }
- 
-function updateButtons() {
-  setTimeout(()=>{
-  toggleDeviceFeature(null, null,  false);
-  updateButtons();
-  },
-  5000);
-}
-
-
-  </script>
-
-		
 <?php
 		$deviceFeatureIdArray = explode(",", $deviceFeatureIds);
 		for($i = 0; $i< count($deviceFeatureIdArray); $i++) {
@@ -149,7 +65,7 @@ function updateButtons() {
       $hashedEntries =  hash_hmac('sha256', $name . $table . $primaryKeyName . $primaryKeyValue, $encryptionPassword);
   ?>
 
-  <div style='margin: 15px'><button id="toggleButton_<?php echo $deviceFeatureId;?>">Toggle Feature</button><div style='font-size:10px;color:#999999' id='statedisplay_<?php echo $deviceFeatureId;?>'></div></div>
+  <div style='margin: 15px'><button class="smart-button" id="toggleButton_<?php echo $deviceFeatureId;?>">Toggle Feature</button><div style='font-size:10px;color:#999999' id='statedisplay_<?php echo $deviceFeatureId;?>'></div></div>
 
   <script>
     var state = false; // start as OFF
@@ -160,21 +76,13 @@ function updateButtons() {
       toggleDeviceFeature(<?php echo $deviceFeatureId;?>, '<?php echo $hashedEntries;?>', true);
     });
     
-     updateButtons();
+    updateFeatureDetailButtons();
   </script>
    <?php
   }
   ?> 
-  
-
-
-
-
-	</div>
-
-
+ 
 </body>
-
 </html>
  
 
