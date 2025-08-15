635f43c173159866bc99f1aa695b08e453c54051
Gus Mueller <gus@asecular.com>
2025-08-15 08:44:01 -0400

    code cleanup on aisle three

--- map.php
+++ map.php
@@ -22,8 +22,8 @@ $scaleConfig =  timeScales();
 $credential = getCredential($user, "googlemap");
 $error = "";
 $deviceId = gvfw("device_id");
-if($deviceId == 0){
-  $deviceId = 21;
+if($deviceId == ""){
+  $deviceId = 21; //hard coded for me, probably not what you want
 }
 if(!$credential) {
 	$error = "No Google API Key found.";
@@ -39,8 +39,6 @@ if ($action == "login") {
 	logOut();
 	header("Location: ?action=login");
 	die();
-	
-
 }
 if(!$user) {
 	if(gvfa("password", $_POST) != "" && $tenantSelector == "") {
@@ -54,16 +52,12 @@ if(!$user) {
 	echo bodyWrap($content, $user, "", null);
 	die();
 }
-
 ?>
 <html>
 <head>
   <title>Maps</title>
   <?php
   if(!$error){?>
-
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@2.0.0/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
   <script>
 	let scaleConfig = JSON.parse('<?php echo json_encode(timeScales()); ?>');
 	window.timezone ='<?php echo $timezone ?>';
@@ -82,18 +76,14 @@ if(!$user) {
     }
   </style
 </head>
-
 <body>
-
 <?php
-
 	$out .= topmostNav();
 	$out .= "<div class='logo'>Map</div>\n";
 	if($user) {
 		$out .= "<div class='outercontent'>";
 		if($poser) {
 			$poserString = " posing as <span class='poserindication'>" . $poser["email"] . "</span> (<a href='?action=disimpersonate'>unpose</a>)";
-
 		}
 		$out .= "<div class='loggedin'>You are logged in as <b>" . $user["email"] . "</b>" .  $poserString . "  on " . $user["name"] . " <div class='basicbutton'><a href=\"?action=logout\">logout</a></div></div>\n";
 		}
@@ -103,62 +93,40 @@ if(!$user) {
 		} 
 		$out .= "<div>\n";
 		$out .= "<div class='documentdescription'>";
-		
- 
 		$out .= "</div>";
 		//$out .= "<div class='innercontent'>";
 		echo $out; 
     $selectId = "locationDropdown";
     $handler = "initMap()";
   ?>
-
     <div style="text-align:center;"><b><span id='greatestTime'></span></b></div>
 		<div class='generalerror'><?php echo $error ?></div>
 		<div id="map"></div>
-
-
-    
     <div style='display:inline-block;vertical-align:top' >
- 
       <div  id='singleplotdiv'>
         <table id="dataTable">
-        
         <?php 
         echo "<tr><td>Time Scale:</td><td>";
         echo genericSelect("scaleDropdown", "scale", defaultFailDown(gvfw("scale"), "day"), $scaleConfig, "onchange", $handler);
         echo "</td></tr>";
         echo "<tr><td>Date/Time Begin:</td><td id='placeforscaledropdown'></td></tr>";
-        //lol, it's easier to specify an object in json and decode it than it is just specify it in PHP
-
         $thisDataSql = "SELECT location_name as text, device_id as value FROM device WHERE location_name <> '' AND location_name IS NOT NULL AND tenant_id=" . intval($user["tenant_id"]) . " AND can_move=1 ORDER BY location_name ASC;";
         $result = mysqli_query($conn, $thisDataSql);
         if($result) {
           $selectData = mysqli_fetch_all($result, MYSQLI_ASSOC); 
         }
-
-        //$selectData = json_decode('[{"text":"Outside Cabin","value":1},{"text":"Cabin Downstairs","value":2},{"text":"Cabin Watchdog","value":3}]');
-        //var_dump($selectData);
-        //echo  json_last_error_msg();
-        
         echo "<tr><td>Tracker:</td><td>" . genericSelect($selectId, "deviceId", defaultFailDown(gvfw("device_id"), $deviceId), $selectData, "onchange", $handler) . "</td></tr>";
         ?>
         </table>
     </div>
 	</div>
-
 </div>
 <script src='map.js'></script>
  <script>
     let map;
     let currentStartDate; //a global that needs to persist through HTTP sessions in the frontend
     let justLoaded = true;
-    let defaultDeviceId = <?php echo $deviceId?>;
-
   </script>
     <script async src="https://maps.googleapis.com/maps/api/js?key=<?php echo $credential["password"]?>&callback=initMap"></script>
 </body>
-
 </html>
- 
-
- 
