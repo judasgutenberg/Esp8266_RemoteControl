a1097f2475dd41441016934fb83bf3d254184fad
Gus Mueller <gus@asecular.com>
2025-12-09 17:35:52 -0500

    Update config.cpp

--- firmware/esp8266/config.cpp
+++ firmware/esp8266/config.cpp
@@ -1,24 +1,22 @@
 #include "config.h"
-#include "globals.h"
-
 char* cs[CONFIG_STRING_COUNT]; //used for string configurations
 int ci[CONFIG_TOTAL_COUNT]; //used for int configurations
 
 void initConfig(void) {
-  cs[STORAGE_PASSWORD] = "your_secret";  // must match storage_password in backend
+  cs[STORAGE_PASSWORD] = "your_storage_password";  // must match storage_password in backend
   cs[URL_GET] = "/weather/server.php";
-  cs[HOST_GET] = "your_server.com";
-  cs[ENCRYPTION_SCHEME] = "ABCDEF0123456789"; // 16-char hex string
+  cs[HOST_GET] = "yourwebsite.com";
+  cs[ENCRYPTION_SCHEME] = "87447CBC22A95125"; // 16-char hex string
   cs[SENSOR_CONFIG_STRING] = ""; // "14*-1*2301*11*0*NULL*stank*1";
-  cs[PINS_TO_START_LOW] = ""; 
-  cs[WIFI_SSID] = "your_ssid";
-  cs[WIFI_PASSWORD] = "password";
-  cs[WIFI_SSID_1] = "your_other_ssid";
-  cs[WIFI_PASSWORD_1] = "could by blank";
-  cs[WIFI_SSID_2] = "wow_a_third_ssid";
-  cs[WIFI_PASSWORD_2] = "";
+  cs[PINS_TO_START_LOW] = ""; // encode as hex if needed
+  cs[WIFI_SSID] = "your_ssid1";
+  cs[WIFI_PASSWORD] = "12345678";
+  cs[WIFI_SSID_1] = "your_ssid2";
+  cs[WIFI_PASSWORD_1] = "12345678";
+  cs[WIFI_SSID_2] = "your_ssid3";
+  cs[WIFI_PASSWORD_2] = "12345678";
   
-  ci[SENSOR_ID] = 280;          // e.g. 2301 = DHT2301
+  ci[SENSOR_ID] = 280;          //we support these: 7410 for ADT7410, 2320 for AHT20, 75 for LM75, 85 for BMP085, 180 for BMP180, 2301 for DHT 2301, 680 for BME680.  0 for no sensor. Multiple sensors are possible.
   ci[SENSOR_I2C] = 0x76;
   ci[CONSOLIDATE_ALL_SENSORS_TO_ONE_RECORD] = 1;
   ci[DEVICE_ID] = 11;
@@ -42,9 +40,9 @@ void initConfig(void) {
   ci[SENSOR_POWER_PIN] = 12;
   ci[SENSOR_SUB_TYPE] = 21;
   ci[IR_PIN] = 14;
-  ci[INA219_ADDRESS] = 0;//0x40;
-  ci[FRAM_ADDRESS] = 0;//0x50;
-  ci[RTC_ADDRESS] = 0;//0x68;
-  ci[SLAVE_I2C] = 20;
-  ci[SLAVE_PET_WATCHDOG_COMMAND] = 203;
+  ci[INA219_ADDRESS] = 0x40;
+  ci[FRAM_ADDRESS] = 0x50;
+  ci[RTC_ADDRESS] = 0x68;
+  ci[SLAVE_I2C] = 0;
+  ci[SLAVE_PET_WATCHDOG_COMMAND] = 0;
 }
