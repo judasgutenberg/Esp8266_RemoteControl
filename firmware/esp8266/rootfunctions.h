1f1fc5386f2ea5a42da219a8756f6ebafaef102e
Gus Mueller <gus@asecular.com>
2026-05-04 13:14:41 -0400

    Update rootfunctions.h

--- firmware/esp8266/rootfunctions.h
+++ firmware/esp8266/rootfunctions.h
@@ -2,8 +2,9 @@ void compileAndSendDeviceData(const String& weatherData,const String& whereWhenD
 void startWeatherSensors(int sensorIdLocal, int sensorSubTypeLocal, int i2c, int pinNumber, int powerPin);
 void cleanup();
 void notYetDeferred();
-void sendIr(String rawDataStr);
 void rtcWrite(RTCBootInfo &d);
+void sendIr(String rawDataStr);
+
 String weatherDataString(int sensorId, int sensorSubtype, int dataPin, int powerPin, int i2c, int deviceFeatureId, char objectCursor, String sensorName, int ordinalOfOverwrite, int consolidateAllSensorsToOneRecord); 
 
 void rtcMarkStable();
